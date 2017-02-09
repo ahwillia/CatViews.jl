950dcdaf34494209e739470bb1fb66d76800dc18
WooKyoung Noh <wookay.noh@gmail.com>
2017-02-10 05:48:20 +0900

    fix the deprecated warnings by Base.repeated on Julia 0.6

--- src/splitview.jl
+++ src/splitview.jl
@@ -6,7 +6,7 @@
     quote
       len = 0
       @nexprs $N (n)->(s_n = len+1; e_n = len+prod(arr[n]); len = e_n) 
-      x = Array(T,len)
+      x = Array{T}(len)
       X = @ntuple $N (n)->(reshape(view(x,s_n:e_n),arr[n]))
       start = @ntuple $N (n)->(s_n)
       stop = @ntuple $N (n)->(e_n)
