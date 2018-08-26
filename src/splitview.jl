1128264400a7ef8c8dcf1befa6867a05bed2a4bc
Robin Deits <robin.deits@gmail.com>
2018-08-26 18:33:07 -0400

    drop Julia v0.6 and support v0.7 and v1.0

--- src/splitview.jl
+++ src/splitview.jl
@@ -1,12 +1,12 @@
 @inline splitview(a::Tuple...) = splitview(Float64,a)
-@inline splitview{T}(::Type{T},a::Tuple...) = splitview(T,a)
+@inline splitview(::Type{T},a::Tuple...) where {T} = splitview(T,a)
 @inline splitview(x::AbstractVector,a::Tuple...) = splitview(x,a)
 
-@generated function splitview{T,N}(::Type{T},arr::NTuple{N,Tuple})
+@generated function splitview(::Type{T},arr::NTuple{N,Tuple}) where {T,N}
     quote
       len = 0
       @nexprs $N (n)->(s_n = len+1; e_n = len+prod(arr[n]); len = e_n)
-      x = Array{T}(len)
+      x = Array{T}(undef, len)
       X = @ntuple $N (n)->(reshape(view(x,s_n:e_n),arr[n]))
       start = @ntuple $N (n)->(s_n)
       stop = @ntuple $N (n)->(e_n)
@@ -14,7 +14,7 @@
     end
 end
 
-@generated function splitview{T,N}(x::AbstractVector{T},arr::NTuple{N,Tuple})
+@generated function splitview(x::AbstractVector{T},arr::NTuple{N,Tuple}) where {T,N}
     quote
       len = 0
       @nexprs $N (n)->(s_n = len+1; e_n = len+prod(arr[n]); len = e_n)
@@ -30,16 +30,16 @@ const FastContiguousSubArray{T,N,P,I<:Tuple{Union{Colon, UnitRange}, Vararg{Any}
 
 parentindex(A::FastContiguousSubArray, i::Int) = A.offset1 + i
 parentindex(A::FastContiguousSubArray, i::Int...) = A.offset1 + prod(i)
-parentindex{N}(A::FastContiguousSubArray, i::NTuple{N,Int}) = A.offset1 + prod(i)
+parentindex(A::FastContiguousSubArray, i::NTuple{N,Int}) where {N} = A.offset1 + prod(i)
 
-@inline parentindex(A::ReshapedArray, i::Int...) = sub2ind(size(A), i...)
-parentindex{N}(A::ReshapedArray, i::NTuple{N,Int}) = sub2ind(size(A), i...)
+@inline parentindex(A::ReshapedArray, i::Int...) = LinearIndices(size(A))[i...]
+parentindex(A::ReshapedArray, i::NTuple{N,Int}) where {N} = LinearIndices(size(A))[i...]
 
 vecidx(A::Array, i::Int) = i
 
 vecidx(A::FastContiguousSubArray, i::Int) = vecidx(A.parent, parentindex(A, i))
 @inline vecidx(A::FastContiguousSubArray, i::Int...) = vecidx(A.parent, parentindex(A, i...))
-vecidx{N}(A::FastContiguousSubArray, i::NTuple{N,Int}) = vecidx(A.parent, parentindex(A, i))
+vecidx(A::FastContiguousSubArray, i::NTuple{N,Int}) where {N} = vecidx(A.parent, parentindex(A, i))
 
 @inline vecidx(A::ReshapedArray, i::Int...) = vecidx(A.parent, parentindex(A, i...))
-vecidx{N}(A::ReshapedArray, i::NTuple{N,Int}) = vecidx(A.parent, parentindex(A, i))
+vecidx(A::ReshapedArray, i::NTuple{N,Int}) where {N} = vecidx(A.parent, parentindex(A, i))
